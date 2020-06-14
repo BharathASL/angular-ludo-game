8ab4bf744554732c97a361bca8ccb8c67cbc5cb6
BharathASL <bharathasl74185@gmail.com>
2020-06-15 04:40:01 +0530

    3 files modified, 4 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   ArrayClassType = Array;
+  board: Array<Object>;
+  constructor() {
+    const temp = Array(4).fill(
+      AppComponent.makeCopy({ location: null, red_carpet: null })
+    );
+    this.board = Array(4).fill(AppComponent.makeCopy(temp));//[red, green, blue, yellow] players by index
+  }
+
+  static makeCopy<T>(target: T): T {
+    return JSON.parse(JSON.stringify(target));
+  }
 }
---
+++ src/app/playable-cell/playable-cell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playable-cell',
+  templateUrl: './playable-cell.component.html',
+  styleUrls: ['./playable-cell.component.css']
+})
+export class PlayableCellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
